2a7af353c3878d22d6691bb6c7bbd09dcd44b518
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2020-11-07 02:06:30 -0500

    suxdb fixture

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
 
+from suxsom.db import DB
 import pytest
 
 @pytest.fixture
-def foo():
-    return []
+def suxdb(tmpdir):
+    db = DB(tmpdir / "test.db")
+    db.create_tables()
+    return db
--- test/test_create_db.py
+++ test/test_create_db.py
@@ -1,13 +1,10 @@
 from suxsom.db import DB
 import sys
 
-def test_null():
-    assert(True)
-
-def test_foo(foo):
-    assert(len(foo) == 0)
-
 def test_create_db(tmpdir):
     db = DB(tmpdir / "test.db")
     db.create_tables()
+    assert(True)
+
+def test_poo(suxdb):
     assert(True)
