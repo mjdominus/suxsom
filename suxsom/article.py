1db009efe71b13a7b8285a26c6d62e5c4dd9a2fb
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2022-01-21 10:40:15 -0500

    2020-11-19 work on article processor

--- suxsom/article.py
+++ suxsom/article.py
@@ -6,36 +6,47 @@ import re
 import suxsom
 
 class Article(sux):
+
     @classmethod
     def owner(cls):
         return "article"
 
     @classmethod
     def from_file(cls, path, prefix):
-        with open(filename, "r") as fh:
-            if path.startswith(prefix):
-                shortpath = path[ len(prefix): ]
+        if path.startswith(prefix):
+            shortpath = path[ len(prefix): ]
+        else:
+            shortpath = path
+
+        with open(path, "r") as fh:
+            line1 = fh.getline()
+            if line1 == "META\n":
+                meta = cls.load_metainfo(fh)
             else:
-                shortpath = filename
+                meta = suxsom.meta.Meta({ "title": line1 })
 
-            meta = suxsom.meta.Meta(cls.parse_metainfo(fh))
-            content = fh.read()
-            last_modified = figure_out_last_modified(filename)
-            return suxsom.sux.Sux(name=shortpath,
-                                  owner=self.owner(),
-                                  last_modified=last_modified,
-                                  meta=meta)
+            return cls.from_string(fh.read(), shortpath, meta, cls.figure_out_last_modified(path, prefix))
 
-    def parse_metainfo(cls, fh):
-        line1 = fh.readline().rstrip()
-        if line1 != "META":
-            fh.readline()       # discard blank line
-            return suxsom.meta.Meta({ "title": line1 })
+    @classmethod
+    def from_string(cls, s, name, meta=None, last_modified=None, **kwargs):
+        if meta is None:
+            meta = suxsom.meta.Meta()
+        meta["content"] = s
 
+        return Article(name=name,
+                       owner=self.owner(),
+                       meta=meta,
+                       last_modified=last_modified,
+                       **kwargs)
+
+    def load_metainfo(cls, fh):
         meta = {}
         while True:
             line = fh.readline().rstrip()
+            if re.search(r'\S', line):
+                break
             match = re.match(r'(.*?): (.*)', line)
             key, value = match.groups()
-            meta[key] = value
+            meta[key.lower()] = value
+
         return suxsom.meta.Meta(meta)
