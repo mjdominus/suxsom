34e5acd2251118930cf25a391d10e76cf5cef564
Mark Jason Dominus <mjd@fastmailteam.com>
2025-05-11 18:28:44 -0400

    Add plugin table
    Sux "owner" field now references plugin by ID, not by name

--- SCHEMA/sux.sql
+++ SCHEMA/sux.sql
@@ -2,7 +2,8 @@ CREATE TABLE `sux` (
   id integer PRIMARY KEY,
   name varchar(64) NOT NULL,
   type varchar(16) NOT NULL,
-  owner varchar(16) NOT NULL,
+  owner_id integer NOT NULL,
   last_modified integer NOT NULL,    -- epoch seconds
-  UNIQUE(name, owner)
+  UNIQUE(name, owner);
+  FOREIGN KEY(owner_id) REFERENCES plugin(id)
 )
